dfd916e9f4852fc2cfbb14a9720c7b0181c77565
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-04-25 21:37:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each do |holiday, supplies|
-      puts "#{holiday.to_s.split("_").map{|x| x.capitalize}.join("):")
+      puts "#{holiday.to_s.split("_").map{|x| x.capitalize}.join("")}"
   
   
   # iterate through holiday_hash and print items such that your readout resembles:
@@ -68,7 +68,8 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+    end 
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
