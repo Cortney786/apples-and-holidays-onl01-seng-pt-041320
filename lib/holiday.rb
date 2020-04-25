4252569666988f27197c90b74c7d4bbabdbcd541
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-04-25 21:49:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,7 +78,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 end 
-end 
+end.flatten.contact 
 end
 
 
